42dcea8bdb8e4d4c981cb24d8a2b1176d8edab4c
Andy Dudley <andy@acornitservices.co.uk>
2019-10-08 18:07:58 +0100

    Add project files.

---
+++ ShapeSorter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShapeSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Triangle tri = new Triangle(3, 5);
+            Square sq = new Square(5);
+            Circle circ = new Circle(1);
+
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(circ);
+            shapes.Add(tri);
+            shapes.Add(sq);
+
+            shapes.Sort();
+
+        }
+    }
+}
---
+++ ShapeSorter/Shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ShapeSorter
+{
+    public class Shape : IComparable
+    {
+        protected internal double _area;
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            Shape otherShape = obj as Shape;
+            if (otherShape != null)
+                return this._area.CompareTo(otherShape._area);
+            else throw new Exception("Object is not a shape");
+;        }
+    }
+
+    public class Square : Shape {
+        public Square(double side)
+        { _area = side * side; }
+    }
+    public class Triangle : Shape {
+        public Triangle(double baselength, double height)
+        { _area = baselength * (height / 2); }
+    }
+    public class Circle : Shape {
+        public Circle(double radius) {
+            _area = (radius*radius) * Math.PI;
+        }
+    }
+    public class Rectangle : Shape {
+        public Rectangle(double width, double height) {
+            _area = width * height;
+        }
+    }
+    public class CustomShape : Shape {
+        public CustomShape (double area) {
+            _area = area;
+        }
+    }
+
+
+}
